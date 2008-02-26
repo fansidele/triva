7c03dcb756f42ec448a8f6bc0805c85ddcd5b25d
schnorr <schnorr@96ca98c3-5b99-4e05-a768-75e2d8bf8841>
2008-02-26 14:44:51 +0000

    L. Schnorr
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/triva@62 96ca98c3-5b99-4e05-a768-75e2d8bf8841

--- trunk/src/core/AppController.h
+++ trunk/src/core/AppController.h
@@ -1,45 +1,5 @@
-#ifndef __PROTOCONTROLLER_H
-#define __PROTOCONTROLLER_H
-
-#ifndef TRIVAWXWIDGETS
-
-#include <Foundation/Foundation.h>
-#include "reader/ProtoReader.h"
-#include "ogre-simulator/OgreProtoSimulator.h"
-#include "memory/ProtoMemory.h"
-#include "view/ProtoView.h"
-
-@interface ProtoController : NSObject
-{
-	ProtoReader *reader;
-	OgreProtoSimulator *simulator;
-	ProtoMemory *memory;
-	ProtoView *view;
-
-	BOOL sessionStarted;
-	BOOL quit;
-
-	NSString *syncfile;
-	NSArray *tracefile;
-}
-- (void) start;
-- (id) initWithArgc: (int) argc andArgv: (char **) argv;
-
-//HACK (workaround to receive trace file names as parameters, instead of using
-//a GUI that does not exist)
-- (void) setSyncfile: (NSString *) f;
-- (void) setTracefile: (NSArray *) a;
-- (NSString *) syncfile;
-- (NSArray *) tracefile;
-
-/* Commands: just to see (initialized|notinitialized) */
-- (BOOL) startSession;
-- (BOOL) endSession;
-//- (void) applicationWillFinishLaunching: (NSNotification *)not;
-@end
-
-
-#else // TRIVAWXWIDGETS
+#ifndef __APPCONTROLLER_H
+#define __APPOCONTROLLER_H
 
 #include <Foundation/Foundation.h>
 #include <wx/wxprec.h>
@@ -53,7 +13,5 @@ class ProtoController : public wxApp
 	public:
 		virtual bool OnInit();
 };
-
-#endif // TRIVAWXWIDGETS
 
 #endif
