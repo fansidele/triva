8cfdebaf463fdccb7651ea674f25088db243479a
schnorr <schnorr@96ca98c3-5b99-4e05-a768-75e2d8bf8841>
2008-02-27 15:07:49 +0000

    L. Schnorr - removing comments
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/triva@77 96ca98c3-5b99-4e05-a768-75e2d8bf8841

--- trunk/src/view/ProtoView.h
+++ trunk/src/view/ProtoView.h
@@ -3,19 +3,11 @@
 #include "view/camera/CameraManager.h"
 #include <Foundation/Foundation.h>
 #include <Ogre.h>
-//#include <OIS.h>
 #include <wx/wx.h>
 #include "general/ProtoComponent.h"
 #include "view/ambient/AmbientManager.h"
-//#include "view/cegui/CEGUIManager.h"
-//#include "view/keyboard/KeyboardListener.h"
-//#include "view/mouse/MouseListener.h"
 #include "view/draw/DrawManager.h"
-//#include "view/exit/ExitManager.h"
-//#include "core/ProtoController.h"
 #include "ogre-simulator/XObject.h"
-
-///enum ProtoApplicationState {Initialized,Configured,Running,Paused};
 
 @interface ProtoView  : ProtoComponent
 {
