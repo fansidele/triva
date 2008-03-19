bcbe718e70363a8c8ad055850ad29e8990c067d8
schnorr <schnorr@96ca98c3-5b99-4e05-a768-75e2d8bf8841>
2008-03-19 15:21:32 +0000

    adding initial support to draw links
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/triva@151 96ca98c3-5b99-4e05-a768-75e2d8bf8841

--- trunk/src/draw/DrawManager+TimestampedObjects.mm
+++ trunk/src/draw/DrawManager+TimestampedObjects.mm
@@ -79,6 +79,67 @@ cString]), ogreColor);
 //				[st redraw];
 //				[ret addState:st];
 			}
+		}else if ([et isKindOfClass: [PajeLinkType class]]){
+			NSEnumerator *en4;
+			PajeEntity *ent;
+		
+			en4 = [viewController enumeratorOfEntitiesTyped:et
+					inContainer:entity
+					fromTime:[viewController startTime]
+					toTime:[viewController endTime]
+					minDuration: 0];
+			while ((ent = [en4 nextObject]) != nil) {
+				Ogre::SceneNode *ssn;
+				ssn = n->createChildSceneNode();
+				NSString *ide = [NSString stringWithFormat:
+@"%@-%@-#-#-%d", [[ent sourceContainer] name], [[ent destContainer] name],
+count++];
+				DynamicLines *ste;
+				std::string n;
+				n = std::string ([ide cString]);
+
+				NSString *sn = [[ent sourceContainer] name];
+				NSString *dn = [[ent destContainer] name];
+
+			        Ogre::Vector3 op = mSceneMgr->getSceneNode ([sn
+cString])->getWorldPosition();
+				Ogre::Vector3 dp = mSceneMgr->getSceneNode ([dn
+cString])->getWorldPosition();
+
+				Ogre::Vector3 dif = dp - op;
+				
+				double start;
+				double end;
+				start = [[[ent time] description] doubleValue];
+				end = [[[ent endTime] description] doubleValue];
+
+				try {
+//					ste = mSceneMgr->getMovableObject(n);
+				}catch (Ogre::Exception ex){
+				}
+				ste = new DynamicLines(Ogre::RenderOperation::OT_LINE_LIST);
+
+				NSColor *color = [viewController colorForEntity:
+ent];
+//				Ogre::ColourValue ogreColor = Ogre::ColourValue([color redComponent], [color greenComponent], [color blueComponent], [color alphaComponent]);
+				Ogre::ColourValue ogreColor =
+Ogre::ColourValue::White;
+				this->createMaterial(std::string([[ent name]
+cString]), ogreColor);
+//				ste->setName (n);
+				ste->setMaterial ([[ent name] cString]);
+				ste->addPoint (op.x, start,op.z);
+				ste->addPoint (dp.x, end, dp.z);
+				ste->update();
+				ste->setQueryFlags (LINK_MASK);
+				ssn->attachObject (ste);
+
+				NSLog (@"ent=%@, source=%@ dest=%@", [ent
+class],[[ent sourceContainer] name], [[ent destContainer] name]);
+
+
+
+			}
 		}
 	}
 }
