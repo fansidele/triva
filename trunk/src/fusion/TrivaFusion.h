a88cec892df94c826107bdc562cced0ae07673de
schnorr <schnorr@96ca98c3-5b99-4e05-a768-75e2d8bf8841>
2008-04-07 14:17:26 +0000

    adding filter named "Fusion", so we will be able to start merging containers
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/triva@230 96ca98c3-5b99-4e05-a768-75e2d8bf8841

---
+++ trunk/src/fusion/TrivaFusion.h
@@ -0,0 +1,11 @@
+#ifndef __TRIVAFUSION_H
+#define __TRIVAFUSION_H
+#include <Foundation/Foundation.h>
+#include <General/PajeFilter.h>
+
+@interface TrivaFusion  : PajeFilter
+{
+}
+@end
+
+#endif
---
+++ trunk/src/fusion/TrivaFusion.m
@@ -0,0 +1,11 @@
+#include "TrivaFusion.h"
+
+@implementation TrivaFusion
+- (id)initWithController:(PajeTraceController *)c
+{
+	self = [super initWithController: c];
+	if (self != nil){
+	}
+	return self;
+}
+@end
