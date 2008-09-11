7184c99d305698ac7c8ac1381953d71e2825047b
schnorr <schnorr@96ca98c3-5b99-4e05-a768-75e2d8bf8841>
2008-09-11 15:08:05 +0000

    read trace data every 10 milliseconds
    details:
    - this can be problematic when multiple events are
    generated at a time by dimvisual
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/triva@417 96ca98c3-5b99-4e05-a768-75e2d8bf8841

--- trunk/src/gui/TrivaController+States.mm
+++ trunk/src/gui/TrivaController+States.mm
@@ -17,7 +17,7 @@ void TrivaController::applicationIsRunning()
 {
 	std::cout << __FUNCTION__ << std::endl;
 	readTimer.SetOwner(this);
-	bool x =readTimer.Start(100,wxTIMER_CONTINUOUS); /* TODO: make 1 second configurable */
+	bool x =readTimer.Start(10,wxTIMER_CONTINUOUS); /* TODO: make 1 second configurable */
 	std::cout << "start result if " << x << std::endl;
 	this->Connect (wxID_ANY, wxEVT_TIMER, wxTimerEventHandler(TrivaController::checkRead));
 NS_DURING
