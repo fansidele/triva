30ba7112d16d20dfc16ef947a1d77da22e1d21a0
schnorr <schnorr@96ca98c3-5b99-4e05-a768-75e2d8bf8841>
2008-11-10 01:40:01 +0000

    removing log messages
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/triva@456 96ca98c3-5b99-4e05-a768-75e2d8bf8841

--- trunk/src/gui/Triva2DFrame.mm
+++ trunk/src/gui/Triva2DFrame.mm
@@ -55,47 +55,39 @@ Triva2DFrame::~Triva2DFrame()
 
 void Triva2DFrame::Init()
 {
-	std::cout << __FUNCTION__ << std::endl;
 }
 
 void Triva2DFrame::Update()
 {
-	std::cout << __FUNCTION__ << std::endl;
 }
 
 void Triva2DFrame::OnSize(wxSizeEvent& evt)
 {
-	std::cout << __FUNCTION__ << std::endl;
 	evt.Skip();
 }
 
 void Triva2DFrame::OnMouseEvent(wxMouseEvent& evt)
 {
-	std::cout << __FUNCTION__ << std::endl;
 	evt.Skip();
 }
 
 void Triva2DFrame::OnMouseCapureLost(wxMouseCaptureLostEvent& evt)
 {
-	std::cout << __FUNCTION__ << std::endl;
 	evt.Skip();
 }
 
 void Triva2DFrame::OnCharEvent(wxKeyEvent& evt)
 {
-	std::cout << __FUNCTION__ << std::endl;
 	evt.Skip();
 }
 
 void Triva2DFrame::OnKeyDownEvent(wxKeyEvent& evt)
 {
-	std::cout << __FUNCTION__ << std::endl;
 	evt.Skip();
 }
 
 void Triva2DFrame::OnKeyUpEvent(wxKeyEvent& evt)
 {
-	std::cout << __FUNCTION__ << std::endl;
 	evt.Skip();
 }
 
