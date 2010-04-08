efbdf304a87fdbe03044528b0eeb92fd0ff9661a
schnorr <schnorr@96ca98c3-5b99-4e05-a768-75e2d8bf8841>
2010-04-08 07:27:24 +0000

    typefilter
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/triva@901 96ca98c3-5b99-4e05-a768-75e2d8bf8841

---
+++ TypeFilter/TimeInterval.h
@@ -0,0 +1,79 @@
+/*
+    This file is part of Triva.
+
+    Triva is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Triva is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Triva.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __TIMEINTERVAL_H
+#define __TIMEINTERVAL_H
+#include <Foundation/Foundation.h>
+#include <General/PajeFilter.h>
+#include "SliceView.h"
+
+@interface TimeInterval  : PajeFilter
+{
+  IBOutlet id traceEndTimeLabel;
+  IBOutlet id sliceView;
+  IBOutlet id traceStartTimeLabel;
+  IBOutlet id forwardLabel;
+  IBOutlet id frequencyLabel;
+  IBOutlet id playButton;
+  IBOutlet id forwardSlider;
+  IBOutlet id frequencySlider;
+  IBOutlet id applyButton;
+  IBOutlet id sizeSlider;
+  IBOutlet id startSlider;
+  IBOutlet id timeSelectionSize;
+  IBOutlet id timeSelectionStart;
+  IBOutlet id updateOnChange;
+  id window;
+
+  double selStart;
+  double selEnd;
+
+  NSTimer *timer;
+}
+- (void) setTimeIntervalFrom: (double) start to: (double) end;
+- (void) updateLabels;
+- (void) apply;
+- (void) animate;
+
+- (void) apply: (id)sender;
+- (void) play: (id)sender;
+- (void) forwardSliderChanged: (id)sender;
+- (void) frequencySliderChanged: (id)sender;
+- (void) sliceSliderChanged: (id)sender;
+
+/*
+{
+
+	BOOL enable;
+	BOOL animationIsRunning;
+	double frequency, forward;
+}
+// callbacks 
+- (void) animationSliderChanged;
+- (void) sliderChanged;
+- (void) preciseSliceEntered;
+- (void) apply;
+- (BOOL) play;
+- (BOOL) pause;
+- (void) animate;
+
+// other methods 
+- (void) setTimeIntervalFrom: (double) start to: (double) end;
+- (double) traceTimeForSliderPosition: (int) position withSize: (double) size;
+*/
+@end
+
+#endif
