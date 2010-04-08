51503b11f428b883fcbd88064d5637370dbdf7ed
schnorr <schnorr@96ca98c3-5b99-4e05-a768-75e2d8bf8841>
2010-04-08 06:52:17 +0000

    cleaning code not used
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/triva@736 96ca98c3-5b99-4e05-a768-75e2d8bf8841

--- GraphView/Graph.mm
+++ GraphView/Graph.mm
@@ -1,10 +1,6 @@
 #include "Graph.h"
 #include "GraphDraw.h"
 
-#define ROUTER_SIZE 0.1
-#define MIN_HOST_SIZE 0.3
-#define MIN_LINK_SIZE 0.01
-
 GraphDraw *draw = NULL;
 
 @implementation Graph
@@ -18,12 +14,6 @@ GraphDraw *draw = NULL;
 	draw = window->getDraw();
 	draw->setController (self);
 	return self;
-}
-
-- (void) timeSelectionChanged
-{
-	NSLog (@"%s:%d", __FUNCTION__, __LINE__);
-	NSLog (@"%@ -> %@", [self selectionStartTime], [self selectionEndTime]);
 }
 
 - (void) hierarchyChanged
