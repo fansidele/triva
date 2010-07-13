7f522524bb6624d3974675920588f5d5fec1e06a
schnorr <schnorr@96ca98c3-5b99-4e05-a768-75e2d8bf8841>
2010-07-13 11:34:50 +0000

    initial implementation for the Compare filter and its controller
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/triva@1093 96ca98c3-5b99-4e05-a768-75e2d8bf8841

---
+++ Compare/Compare.m
@@ -0,0 +1,39 @@
+/*
+    This file is part of Triva.
+
+    Triva is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Triva is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Triva.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "Compare.h"
+
+@implementation Compare
+- (id)initWithController:(PajeTraceController *)c
+{
+  self = [super initWithController: c];
+//  if (self != nil){
+//    [NSBundle loadNibNamed: @"Compare" owner: self];
+//  }
+  return self;
+}
+
+- (void) setController: (CompareController*) c
+{
+  controller = c;
+}
+
+- (void) timeLimitsChanged
+{
+  [controller timeLimitsChangedWithSender: self];
+  [super timeLimitsChanged];
+}
+@end
