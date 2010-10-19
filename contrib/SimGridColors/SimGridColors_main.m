eeb057d3dbd1bf369932cf5ceec0d39d34b0033b
schnorr <schnorr@96ca98c3-5b99-4e05-a768-75e2d8bf8841>
2010-10-19 13:45:37 +0000

    if parameters are passed to SimGridColors, consider them as categories
    details:
    - this sets their colors using the simgrid variable names
    - creates and sets as default a graph configuration for triva
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/triva@1261 96ca98c3-5b99-4e05-a768-75e2d8bf8841

--- contrib/SimGridColors/SimGridColors_main.m
+++ contrib/SimGridColors/SimGridColors_main.m
@@ -27,6 +27,90 @@
 int 
 main(int argc, const char *argv[])
 {
-  return NSApplicationMain (argc, argv);
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  int ret = 0;
+  if (argc != 1){
+    int i;
+    srand48(time(NULL));
+
+    NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *trivaDefaults;
+    trivaDefaults = [[NSMutableDictionary alloc] initWithDictionary:
+              [d persistentDomainForName: @"Triva"]];
+    NSMutableArray *categories = [NSMutableArray array];
+    for (i = 1; i < argc; i++){
+      [categories addObject: [NSString stringWithFormat: @"%s", argv[i]]];
+    }
+
+    NSLog (@"Setting colors for: %@", categories);
+    NSEnumerator *en = [categories objectEnumerator];
+    NSString *category;
+    NSMutableArray *pCategories = [NSMutableArray array];
+    NSMutableArray *bCategories = [NSMutableArray array];
+    while ((category = [en nextObject])){
+      double red = drand48();
+      double green = drand48();
+      double blue = drand48();
+      NSColor *color = [NSColor colorWithCalibratedRed: red
+                                                 green: green
+                                                  blue: blue
+                                                 alpha: 1];
+      NSString *p_category;
+      NSString *b_category;
+      p_category = [NSString stringWithFormat: @"p%@ Color", category];
+      b_category = [NSString stringWithFormat: @"b%@ Color", category];
+      [trivaDefaults setObject: color forKey: p_category];
+      [trivaDefaults setObject: color forKey: b_category];
+
+      [pCategories addObject: [NSString stringWithFormat: @"p%@", category]];
+      [bCategories addObject: [NSString stringWithFormat: @"b%@", category]];
+    }
+
+    NSLog (@"Graph configuration with: %@", categories);    
+    NSString *graphConfKey = @"SimGrid Graph Configuration";
+    NSMutableDictionary *graphConf = [NSMutableDictionary dictionary];
+    NSMutableDictionary *hostConf = [NSMutableDictionary dictionary];
+    NSMutableDictionary *hostConfComp = [NSMutableDictionary dictionary];
+    NSMutableDictionary *linkConf = [NSMutableDictionary dictionary];
+    NSMutableDictionary *linkConfComp = [NSMutableDictionary dictionary];
+
+    [graphConf setObject: [NSArray arrayWithObjects: @"HOST", nil]
+                  forKey: @"node"];
+    [graphConf setObject: [NSArray arrayWithObjects: @"LINK", nil]
+                  forKey: @"edge"];
+    [graphConf setObject: @"neato" forKey: @"graphviz-algorithm"];
+
+    [hostConfComp setObject: @"power" forKey: @"size"];
+    [hostConfComp setObject: @"separation" forKey: @"type"];
+    [hostConfComp setObject: pCategories forKey: @"values"];
+    [hostConf setObject: @"power" forKey: @"size"];
+    [hostConf setObject: @"global" forKey: @"scale"];
+    [hostConf setObject: hostConfComp forKey: @"composition001"];
+    [graphConf setObject: hostConf forKey: @"HOST"];
+  
+    [linkConfComp setObject: @"bandwidth" forKey: @"size"];
+    [linkConfComp setObject: @"separation" forKey: @"type"];
+    [linkConfComp setObject: bCategories forKey: @"values"];
+    [linkConf setObject: @"source" forKey: @"src"];
+    [linkConf setObject: @"destination" forKey: @"dst"];
+    [linkConf setObject: @"bandwidth" forKey: @"size"];
+    [linkConf setObject: @"global" forKey: @"scale"];
+    [linkConf setObject: linkConfComp forKey: @"composition001"];
+    [graphConf setObject: linkConf forKey: @"LINK"];
+
+    NSMutableDictionary *graphConfs;
+    graphConfs = [NSMutableDictionary dictionaryWithDictionary:
+                    [trivaDefaults objectForKey: @"GraphConfigurationItems"]];
+    [graphConfs setObject: [graphConf description] forKey: graphConfKey];
+    [trivaDefaults setObject: graphConfs forKey: @"GraphConfigurationItems"];
+    [trivaDefaults setObject: graphConfKey forKey: @"GraphConfigurationSelected"];
+    [d setPersistentDomain: trivaDefaults forName: @"Triva"];
+    [trivaDefaults release];
+    [d synchronize];
+  }else{
+    ret = NSApplicationMain (argc, argv);
+  }
+  [pool release];
+  return ret;
 }
 
