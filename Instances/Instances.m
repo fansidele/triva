873dcc364f400e274d51629bfadbbf4ae7e33e39
Lucas Schnorr <Lucas.Schnorr@imag.fr>
2011-12-08 09:25:14 +0100

    fixing identation, no changes

--- Instances/Instances.m
+++ Instances/Instances.m
@@ -28,34 +28,34 @@
 - (void)printInstance:(id)instance level:(int)level
 {
 
-    NSLog(@"i%*.*s%@", level, level, "", [self descriptionForEntity:instance]);
-    PajeEntityType *et;
-    NSEnumerator *en;
-    en = [[self containedTypesForContainerType:[self entityTypeForEntity:instance]] objectEnumerator];
-    while ((et = [en nextObject]) != nil) {
-        NSLog(@"t%*.*s%@", level+1, level+1, "", [self descriptionForEntityType:et]);
-        if ([self isContainerEntityType:et]) {
-            NSEnumerator *en2;
-            PajeContainer *sub;
-            en2 = [self enumeratorOfContainersTyped:et inContainer:instance];
-            while ((sub = [en2 nextObject]) != nil) {
-                [self printInstance:sub level:level+2];
-            }
-        } else {
-            NSEnumerator *en3;
-            PajeEntity *ent;
-            NSDate *startt = [NSDate dateWithTimeIntervalSinceReferenceDate: -1];
-            NSDate *endt = [self endTime];
-            en3 = [self enumeratorOfEntitiesTyped:et
-                                      inContainer:instance
-                                         fromTime:startt
-                                           toTime:endt
-                                      minDuration:0];
-            while ((ent = [en3 nextObject]) != nil) {
-                NSLog(@"e%*.*s%@", level+2, level+2, "", [self descriptionForEntity:ent]);
-            }
-        }
+  NSLog(@"i%*.*s%@", level, level, "", [self descriptionForEntity:instance]);
+  PajeEntityType *et;
+  NSEnumerator *en;
+  en = [[self containedTypesForContainerType:[self entityTypeForEntity:instance]] objectEnumerator];
+  while ((et = [en nextObject]) != nil) {
+    NSLog(@"t%*.*s%@", level+1, level+1, "", [self descriptionForEntityType:et]);
+    if ([self isContainerEntityType:et]) {
+      NSEnumerator *en2;
+      PajeContainer *sub;
+      en2 = [self enumeratorOfContainersTyped:et inContainer:instance];
+      while ((sub = [en2 nextObject]) != nil) {
+        [self printInstance:sub level:level+2];
+      }
+    } else {
+      NSEnumerator *en3;
+      PajeEntity *ent;
+      NSDate *startt = [NSDate dateWithTimeIntervalSinceReferenceDate: -1];
+      NSDate *endt = [self endTime];
+      en3 = [self enumeratorOfEntitiesTyped:et
+                                inContainer:instance
+                                   fromTime:startt
+                                     toTime:endt
+                                minDuration:0];
+      while ((ent = [en3 nextObject]) != nil) {
+        NSLog(@"e%*.*s%@", level+2, level+2, "", [self descriptionForEntity:ent]);
+      }
     }
+  }
 }
 
 - (void) dumpTraceInTextualFormat
