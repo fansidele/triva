5103db948e16aff38d6d67f97348f4228c11bed3
Lucas Schnorr <Lucas.Schnorr@imag.fr>
2012-01-26 17:19:10 +0100

    printing extra fields for Instance filter

--- Instances/Instances.m
+++ Instances/Instances.m
@@ -65,7 +65,7 @@
                 [[ent destContainer] name],
                 [ent extraFields]);
         }else{
-          NSLog(@"e%*.*s%@", level+2, level+2, "", [self descriptionForEntity:ent]);
+          NSLog(@"e%*.*s%@ %@", level+2, level+2, "", [self descriptionForEntity:ent], [ent extraFields]);
         }
       }
     }
